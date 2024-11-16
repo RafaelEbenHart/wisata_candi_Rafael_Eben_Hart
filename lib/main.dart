0a29ff86f0596c5a3799ba4424242534bb8ee016
Rafael <RafaelEbenHart_2327240120@mhs.mdp.ac.id>
2024-11-16 09:21:23 +0700

    Navigation
    navigasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi_rafael_eben_hart/screens/favorite_screen.dart';
 import 'package:wisata_candi_rafael_eben_hart/screens/home_screen.dart';
 import 'package:wisata_candi_rafael_eben_hart/screens/profile_screen.dart';
 import 'package:wisata_candi_rafael_eben_hart/screens/search_screen.dart';
@@ -11,9 +12,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -31,14 +37,80 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const MainScreen(),
       //home: ProfileScreen(),
       //home: DetailScreen(candi: candiList[0]),
       //home: SignInScreen(),
       //home: SignUpScreen(),
       //home: SearchScreen(),
-      home: HomeScreen(),
+      //home: HomeScreen(),
     );
   }
 }
 
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //Todo: 1.Deklarasi variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Todo: 2.body
+      body: _children[_currentIndex],
+      //todo: 3.BottomNavigatorBar
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[50]),
+          child: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex=index;
+              });
+            },
+            items: [
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Home"),
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Search"),
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Favorite"),
+              const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Person"),
+            ],
+            selectedItemColor: Colors.deepPurple,
+            unselectedItemColor: Colors.deepPurple[100],
+            showSelectedLabels: true,
+          )),
+      //Todo: 4.data child theme
+    );
+  }
+}
